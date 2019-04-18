4f67df1b28a0787cec8f15c04dc1d80303763d80
Peter Williams <peterwilliams@Peters-MacBook-Pro.local>
2019-04-18 16:50:20 -0600

    Updated Assignment 5 script and graphs/files

--- Assignments/Assignment_5/Assignment_5_script.R
+++ Assignments/Assignment_5/Assignment_5_script.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 
+# load the mtcars dataset
 data("mtcars")
 mtcars
 glimpse(mtcars)
@@ -17,18 +18,19 @@ write.csv(df1, "automatic_mtcars.csv")
 
 #  plots the effect of horsepower on miles-per-gallon (update plot to have meaningful labels and title)
 #  saves this plot as a png image called "mpg_vs_hp_auto.png" in your Assignment_5 directory
-png("mpg_vs_hp_auto.png")
-ggplot(df.aut, aes(x=hp, y=mpg)) +
+
+ggplot(df1, aes(x=hp, y=mpg)) +
   geom_point() + 
   geom_smooth(method = "lm", se = FALSE) +
   ggtitle( "Miles per Gallon ~ Horsepower") +
-  labs(x = "Horsepower", y = "Miles per Gallon") 
-dev.off()
+  labs(x = "Horsepower", y = "Miles per Gallon") +
+  ggsave("./mpg_vs_hp_auto.png")
+
 
 #  plots the effect of weight on miles-per-gallon (with improved labels, again)
 #  saves this second plot as a **tiff** image called "mpg_vs_wt_auto.tiff" in your Assignment_5 directory
 tiff("mpg_vs_wt_auto.tiff")
-ggplot(df.aut, aes(x=wt, y=mpg)) +
+ggplot(df1, aes(x=wt, y=mpg)) +
   geom_point() + 
   geom_smooth(method = "lm", se = FALSE) + 
   ggtitle( "Miles per Gallon ~ Weight") +
@@ -38,21 +40,27 @@ dev.off()
 #  subsets the original mtcars dataframe to include only cars with displacements less than or equal to 200 cu.in.
 #  saves that new subset as a csv file called mtcars_max200_displ.csv
 
-df %>%
-  filter(disp <= 200) %>%
-  write.csv("mtcars_max200_displ.csv")
+df2 = mtcars %>%
+  filter(disp <= 200)
+
+write.csv(df2, "mtcars_max200_displ.csv")
 
 # includes code to calculate the maximum horsepower for each of the three dataframes (original, automatic, max200)
 # prints these calculations (from task 10) in a readable format to a new plaintext file called hp_maximums.txt
-mtcars %>%
+sum1 = mtcars %>%
   summarise(max(hp))
 
-df1 %>%
+sum2 = df1 %>%
   summarise(max(hp))
 
-df2  %>%
+sum3 = df2  %>%
   summarise(max(hp))
 
+sink("./max.horsepower.of.each.df.txt")
+paste("Original =", sum1, "max hp")
+paste("Automatic =", sum2, "max hp")
+paste("Max200 =", sum3, "max hp")
+sink(NULL)
 
 
 
